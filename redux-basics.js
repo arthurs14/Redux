30e16a88e19cd2c5d85810052acd63580d860a30
Arthur Sandro <arthurcs426@gmail.com>
2020-12-28 15:44:59 -0800

    good sample of all the parts of redux

--- redux-basics.js
+++ redux-basics.js
@@ -26,10 +26,13 @@ let rootReducer = (state = initialState, action) => {
 let store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+});
+
 // Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 5 });
 console.log(store.getState());
 
-
-// Subscription
