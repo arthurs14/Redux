1b705ec5110dcfe209a198feee5b4192bb3ffc32
Arthur Sandro <arthurcs426@gmail.com>
2020-12-27 16:48:45 -0800

    showed an example of how dispatch works to update the state through the reducer in the store

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ let initialState = {
 
 // Reducer
 let rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
   return state;
 }
 
@@ -15,5 +27,9 @@ let store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 5 });
+console.log(store.getState());
+
 
 // Subscription
