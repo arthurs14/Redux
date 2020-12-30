d3c431027e280312e4ff8f19fd4eb18b7fe6e7ee
Arthur Sandro <arthurcs426@gmail.com>
2020-12-30 15:59:46 -0800

    got of reducers file in order to divide the reducer into multiple ones

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,32 +1,11 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 let initialState = {
-  counter: 0,
   results: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.INCREMENT:
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
-    case actionTypes.DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
-    case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value,
-      };
-    case actionTypes.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.value,
-      };
     case actionTypes.STORE_RESULT:
       return {
         ...state,
