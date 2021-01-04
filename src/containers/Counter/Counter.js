8aff0037407acf977243d789ebb420f98353103b
Arthur Sandro <arthurcs426@gmail.com>
2021-01-03 16:12:18 -0800

    got rid of a method within the counter class since we take care of it in the reducer

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -10,25 +10,6 @@ class Counter extends Component {
         counter: 0
     }
 
-    counterChangedHandler = (action, value) => {
-        switch (action) {
-            case 'inc':
-                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
-                break;
-            case 'dec':
-                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
-                break;
-            case 'add':
-                this.setState((prevState) => { return { counter: prevState.counter + value } })
-                break;
-            case 'sub':
-                this.setState((prevState) => { return { counter: prevState.counter - value } })
-                break;
-            default:
-                break;
-        }
-    }
-
     render() {
         const { ctr, storedResults, increment, decrement, add, subtract, onStoreResult, onDeleteResult } = this.props;
         return (
