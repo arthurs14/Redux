c34a4cbeceef7f2785c716486c41c5579e501d9e
Arthur Sandro <arthurcs426@gmail.com>
2021-01-13 15:37:09 -0800

    moved store actions to its own file

--- src/store/actions/actionsTypes.js
+++ src/store/actions/actionsTypes.js
@@ -4,25 +4,3 @@ export const ADD = 'ADD';
 export const SUBTRACT = 'SUBTRACT';
 export const STORE_RESULT = 'STORE_RESULT';
 export const DELETE_RESULT = 'DELETE_RESULT';
-
-export const saveResult = (result) => {
-  return {
-    type: STORE_RESULT,
-    result: result,
-  };
-}
-
-export const storeResult = (result) => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(saveResult(result));
-    }, 2000);
-  };
-};
-
-export const deleteResult = (id) => {
-  return {
-    type: DELETE_RESULT,
-    id: id,
-  };
-};
