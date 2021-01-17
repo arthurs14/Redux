f04f4a794951de0cd66414cf7ae6b24ebde7730c
Arthur Sandro <arthurcs426@gmail.com>
2021-01-17 14:06:22 -0800

    added utility function to all counter cases

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -8,20 +8,11 @@ let initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INCREMENT:
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
+      return updateObject(state, { counter: state.counter + 1 });
     case actionTypes.DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
+      return updateObject(state, { counter: state.counter - 1 });
     case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value,
-      };
+      return updateObject(state, { counter: state.counter + action.value });
     case actionTypes.SUBTRACT:
       return updateObject(state, { counter: state.counter - action.value });
     default:
