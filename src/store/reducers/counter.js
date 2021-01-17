57d4f17ac71532be135499947cbae4cb52669117
Arthur Sandro <arthurcs426@gmail.com>
2021-01-17 14:04:38 -0800

    added a new file to make our udating of state a little leaner

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 let initialState = {
   counter: 0,
@@ -22,10 +23,7 @@ const reducer = (state = initialState, action) => {
         counter: state.counter + action.value,
       };
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.value,
-      };
+      return updateObject(state, { counter: state.counter - action.value });
     default:
       return state;
   }
