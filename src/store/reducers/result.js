678fee4678b87a7f6c6c83961fadba960d8bf3f9
Arthur Sandro <arthurcs426@gmail.com>
2021-01-18 15:34:17 -0800

    tried to see if I could put the case statements on one line but then breaks would of been needed

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -12,9 +12,9 @@ const deleteResult = (state, action) => {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
+    case actionTypes.STORE_RESULT: 
       return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) });
-    case actionTypes.DELETE_RESULT:
+    case actionTypes.DELETE_RESULT: 
       return deleteResult(state, action);
     default:
       return state;
