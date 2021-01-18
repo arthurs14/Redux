52c1721b3e4e8ef56f70ba929711f08077917915
Arthur Sandro <arthurcs426@gmail.com>
2021-01-18 15:27:55 -0800

    made switch case more leaner by moving the syntax to a helper function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,15 +5,17 @@ let initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  const updatedArr = state.results.filter(result => result.id !== action.id);
+  return updateObject(state, { results: updatedArr });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) });
     case actionTypes.DELETE_RESULT:
-      // let newArr = [...state.results];
-      // newArr.splice(id, 1);
-      let updatedArr = state.results.filter(result => result.id !== action.id);
-      return updateObject(state, { results: updatedArr });
+      return deleteResult(state, action);
     default:
       return state;
   }
